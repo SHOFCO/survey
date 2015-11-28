bdf43613927e0d3b19137ac1e7e138e99b4b85ce
Robby Walker <robby.walker@gmail.com>
2015-11-27 17:35:20 -0700

    add schema validation to update step

--- update.py
+++ update.py
@@ -3,6 +3,68 @@
 import json
 import yaml
 
+
+SCHEMA = {
+    '*': {
+        'required': {'label', 'type'},
+        'allowed': {'instructions', 'subs', 'when', 'endSurveyWhen'}
+    },
+    'yesNo': {
+    },
+    'text': {
+        'allowed': {'rows'}
+    },
+    'signature': {},
+    'checkboxes': {
+        'required': {'options'},
+        'allowed': {'other', 'noneOfTheAbove'}
+    },
+    'random': {
+        'required': {'min', 'max'}
+    },
+    'number': {
+        'allowed': {'min', 'max'}
+    },
+    'options': {
+        'required': {'options'},
+        'allowed': {'other'}
+    },
+    'table': {
+        'required': {'keys', 'keyLabel', 'valueLabel'},
+        'allowed': {'valueType'}
+    }
+}
+
+def validateQuestion(question):
+    errors = 0
+    fieldType = question.get('type')
+    label = question.get('label', 'UNLABELED QUESTION')
+    if not fieldType:
+        print 'Question "%s" has no type' % label
+        errors += 1
+        
+    elif fieldType not in SCHEMA:
+        print 'Question "%s" has illegal type' % (label, fieldType)
+        errors += 1
+        
+    else:
+        fields = set(question.keys())
+        required = SCHEMA['*']['required'] | SCHEMA[fieldType].get('required', set())
+        allowed = SCHEMA['*']['allowed'] | SCHEMA[fieldType].get('allowed', set())
+        
+        if required - fields:
+            print 'Question "%s" of type %s is missing fields: %s' % (label, fieldType, required - fields)
+        extra = fields - (required | allowed)
+        if extra:
+            print 'Question "%s" of type %s has extra fields: %s' % (label, fieldType, extra)
+        
+    if 'subs' in question:
+        for sub in question['subs']:
+            errors == validateQuestion(sub)
+
+    return errors
+
+    
 def main():
     try:
         s = open('baseline-2016.yaml').read().replace('-\t', '-   ').replace('\t', '    ')
@@ -12,13 +74,20 @@ def main():
         print exc.problem_mark
         return
     
+    errors = 0
+    for page in raw:
+        if 'questions' in page:
+            for question in page['questions']:
+                errors += validateQuestion(question)
+            
     # TODO: compilation / validation
-    #    - field types (present and valid)
     #    - no overlapping keys
-    #    - correct sub-fields present (schema validation)
-    
-    json.dump(raw, open('viewer/baseline-2016.json', 'w'))
-    print 'Success!'
+
+    if errors:
+        print '%d Errors!' % errors
+    else:
+        json.dump(raw, open('viewer/baseline-2016.json', 'w'))
+        print 'Success!'
     
     
 
